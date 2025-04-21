b4961e60248146a669276b126021b5f7c0e7a6d6
Waldut <syawalalfath29@gmail.com>
2025-04-21 13:03:29 +0700

    feat:add new route

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 Route::get('/dashboard', function () {
-    return view('index');
+    return view('dashboard');
 });
 
-Route::get('/all-tasks', function () {
-    return view('tasks.index');
-});
-Route::get('/all-tasks/create', function () {
-    return view('tasks.create');
-});
-Route::get('/all-tasks/edit', function () {
-    return view('tasks.edit');
-});
+Route::get('/dashboard' ,[TaskController::class, 'dashboard'])->name('dashboard');
+Route::get('/all-tasks' ,[TaskController::class, 'index'])->name('tasks.index');
+
+Route::get('/all-tasks/create' ,[TaskController::class, 'create'])->name('task.create');
+Route::post('/all-tasks' ,[TaskController::class, 'store'])->name('task.store');
+Route::get('/all-tasks/{id}/edit' ,[TaskController::class, 'edit'])->name('task.edit');
+Route::put('/all-tasks/{id}/edit' ,[TaskController::class, 'update'])->name('task.update');
+Route::delete('/all-tasks/{id}/destroy' ,[TaskController::class, 'destroy'])->name('task.destroy');
+Route::patch('/all-tasks/{id}/mark-as-done' ,[TaskController::class, 'markAsDone'])->name('task.markAsDone');
